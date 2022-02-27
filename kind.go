f59daa038bdf4b7792ea1e80fb0e9947900a98a1
Kevin Kreitner <kevinkreitner@gmail.com>
2022-02-27 22:23:45 +0100

    Add IsInStack for Kind

--- kind.go
+++ kind.go
@@ -12,10 +12,30 @@ const (
 )
 
 func Is(err error, kind Kind) bool {
+	if err == nil {
+		return false
+	}
 	e, ok := err.(Error)
 	if !ok {
 		return kind == KindUnexpected
 	}
 
 	return e.Kind() == kind
+}
+
+func IsInStack(err error, kind Kind) bool {
+	if Is(err, kind) {
+		return true
+	}
+
+	e, ok := err.(Error)
+	if !ok {
+		return kind == KindUnexpected
+	}
+
+	if e.Cause() == nil {
+		return false
+	}
+
+	return IsInStack(e.Cause(), kind)
 }
